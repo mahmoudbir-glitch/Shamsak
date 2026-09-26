01ad3d9b4c1bb18c692e8075d367a09ac2920392
mahmoudbir-glitch <mahmoud.bir@gmail.com>
2026-09-26 11:42:13 +0300

    feat: add smart forecasting page

---
+++ src/app/forecast/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { SmartForecast } from "@/components/smart-forecast";
+
+export default function ForecastPage() {
+  return <div className="w-full"><SmartForecast /></div>;
+}
